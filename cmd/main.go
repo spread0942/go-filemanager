ff9f11a8ccb648e79dca4853d29a997f3a6029f4
spread <spread@vbox.a3srl.local>
2025-01-30 09:12:26 -0500

    chore(create): create file or directory

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"filemanager/internal"
+	"os"
+
+	"flag"
+	"fmt"
+)
+
 func main()  {
+	createFlag := flag.String("create", "", "Specify type: a file or dir to create")
+	pathFlag := flag.String("path", ".", "Specify the path where to create the file or directory")
+	nameFlag := flag.String("name", "", "Specify the file or directory name")
+
+	flag.Parse()
+
+	if *createFlag == "" {
+		fmt.Println("[-] Error: You must specify a file or directory to create using -create flag.")
+		os.Exit(1)
+	}
+
+	if *createFlag == "file" {
+		succ, err := utils.CreateFile(*nameFlag, *pathFlag)
+
+		if (succ) {
+			fmt.Println("[+] File created")
+			os.Exit(0)
+		} else {
+			fmt.Println("[-] Error creating file: ", err)
+			os.Exit(1)
+		}
+	} else if *createFlag == "dir" {
+		succ, err := utils.CreateDirectory(*nameFlag, *pathFlag)
+
+		if (succ) {
+			fmt.Println("[+] Directory created")
+			os.Exit(0)
+		} else {
+			fmt.Println("[-] Error creating directory: ", err)
+			os.Exit(1)
+		}
+	}
 	
 }
---
+++ internal/utils.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func CreateFile(filename string, path string) (bool, error) {
+	fullpath := filepath.Join(path, filename)
+	file, err := os.Create(fullpath)
+
+	if (err != nil) {
+		return false, err
+	}
+
+	defer file.Close()
+	return true, nil
+}
+
+func CreateDirectory(directory string, path string) (bool, error) {
+	fullpath := filepath.Join(path, directory)
+	err := os.Mkdir(fullpath, 0755)
+
+	if (err != nil) {
+		return false, err
+	}
+	return true, nil
+}
